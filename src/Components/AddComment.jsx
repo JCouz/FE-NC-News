d48787cbee31b67786b16656e93a6df7c998103e
JCouz <couzensj@live.co.uk>
2022-04-10 14:12:33 +0100

    add ability to delete comment with styling changes

--- src/Components/AddComment.jsx
+++ src/Components/AddComment.jsx
@@ -1,35 +1,49 @@
 import { useState } from "react";
 import * as api from "../api";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Plus } from "react-feather";
 
 export default function AddComment({ setComments, article_id }) {
   // this is a variable, commentInput is declared as '' in useState, setCommentInput is reassigning that variable
   const [commentInput, setCommentInput] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
-    api
-      .addComment(article_id, { body: commentInput, username: "cooljmessy" })
-      .then((comment) => {
-        setCommentInput("");
-        setComments((currentComment) => {
-          return [comment, ...currentComment];
-        });
+    api.addComment(article_id, { body: commentInput, username: "cooljmessy" }).then((comment) => {
+      console.log(comment);
+      setCommentInput("");
+      setComments((currentComment) => {
+        return [comment, ...currentComment];
       });
+    });
   };
   return (
     <form onSubmit={handleSubmit}>
-      <label id="add-comment-title">
-        Add Comment:
-        <input
-          type="text"
-          id="add-comment-box"
-          value={commentInput}
-          onChange={(event) => {
-            setCommentInput(event.target.value);
-          }}
-        />
-      </label>
-
-      <button type="submit">Submit</button>
+      <Container id="add-comment-box">
+        <Row>
+          <Col>
+            <Form onSubmit={handleSubmit} className="mt-2">
+              <Form.Control
+                type="text"
+                placeholder="Add Comment..."
+                value={commentInput}
+                onChange={(event) => {
+                  setCommentInput(event.target.value);
+                }}
+              />
+            </Form>
+          </Col>
+          <Col xs="auto">
+            <Button variant="danger" className="m-2" type="submit">
+              Submit
+              <Plus className="mx-2" />
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </form>
   );
 }
--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,16 +1,59 @@
-import timeAgo from '../utils';
-import CommentVoter from './CommentVoter';
+import timeAgo from "../utils";
+import CommentVoter from "./CommentVoter";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import * as api from "../api";
+import { useState } from "react";
 
-export default function CommentCard({
-  body,
-  author,
-  votes,
-  created_at,
-  comment_id,
-}) {
+export default function CommentCard({ body, author, votes, created_at, comment_id, reloadComments }) {
+  const [deleted, setDeleted] = useState(false);
+
+  function deleteComment() {
+    api.deleteComment(comment_id);
+    setDeleted(true);
+  }
+
+  function renderDelete() {
+    return author === "cooljmessy" ? (
+      <Button variant="danger" size="sm" className="float-right" onClick={() => deleteComment()}>
+        Delete
+      </Button>
+    ) : (
+      ""
+    );
+  }
+
+  if (deleted) {
+    return (
+      <Alert variant="danger" style={{ textAlign: "center" }}>
+        "Comment Deleted"
+      </Alert>
+    );
+  }
+
   return (
     <section className="comment-card">
-      <dl>
+      <Container className="my-1">
+        <Row>
+          <Col>{body}</Col>
+        </Row>
+        <Row>
+          <Col>User: {author}</Col>
+        </Row>
+        <Row>
+          <Col>{timeAgo.format(new Date(created_at))}</Col>
+        </Row>
+        <Row>
+          <Col>
+            <CommentVoter votes={votes} comment_id={comment_id} />
+          </Col>
+          <Col xs="auto">{renderDelete()}</Col>
+        </Row>
+      </Container>
+      {/* <dl>
         <dt>{body}</dt>
         <dt>User: {author}</dt>
 
@@ -19,6 +62,7 @@ export default function CommentCard({
           <CommentVoter votes={votes} comment_id={comment_id} />
         </dt>
       </dl>
+      {renderDelete()} */}
     </section>
   );
 }
