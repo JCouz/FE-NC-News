e306cef579d6e7ea058af1efd7006cd6f10f9f7f
JCouz <couzensj@live.co.uk>
2022-04-11 22:31:36 +0100

    remove excess form for handleSubmit

--- src/Components/AddComment.jsx
+++ src/Components/AddComment.jsx
@@ -1,49 +1,49 @@
-import { useState } from "react";
-import * as api from "../api";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { Plus } from "react-feather";
+import { useState } from 'react';
+import * as api from '../api';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Plus } from 'react-feather';
 
 export default function AddComment({ setComments, article_id }) {
   // this is a variable, commentInput is declared as '' in useState, setCommentInput is reassigning that variable
-  const [commentInput, setCommentInput] = useState("");
+  const [commentInput, setCommentInput] = useState('');
   const handleSubmit = (event) => {
     event.preventDefault();
-    api.addComment(article_id, { body: commentInput, username: "cooljmessy" }).then((comment) => {
-      console.log(comment);
-      setCommentInput("");
-      setComments((currentComment) => {
-        return [comment, ...currentComment];
+    api
+      .addComment(article_id, { body: commentInput, username: 'cooljmessy' })
+      .then((comment) => {
+        console.log(comment);
+        setCommentInput('');
+        setComments((currentComment) => {
+          return [comment, ...currentComment];
+        });
       });
-    });
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <Container id="add-comment-box">
-        <Row>
-          <Col>
-            <Form onSubmit={handleSubmit} className="mt-2">
-              <Form.Control
-                type="text"
-                placeholder="Add Comment..."
-                value={commentInput}
-                onChange={(event) => {
-                  setCommentInput(event.target.value);
-                }}
-              />
-            </Form>
-          </Col>
-          <Col xs="auto">
-            <Button variant="danger" className="m-2" type="submit">
-              Submit
-              <Plus className="mx-2" />
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-    </form>
+    <Container id="add-comment-box">
+      <Row>
+        <Col>
+          <Form onSubmit={handleSubmit} className="mt-2">
+            <Form.Control
+              type="text"
+              placeholder="Add Comment..."
+              value={commentInput}
+              onChange={(event) => {
+                setCommentInput(event.target.value);
+              }}
+            />
+          </Form>
+        </Col>
+        <Col xs="auto">
+          <Button variant="danger" className="m-2" type="submit">
+            Submit
+            <Plus className="mx-2" />
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
